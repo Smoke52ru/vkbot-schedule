c138bb8480c09005582ceb004c143cd4ce3e7714
Dmitry Smirnov <diamond101001@gmail.com>
2021-10-23 00:16:32 +0300

    Модули авторизации и админка

---
+++ permissions.py
@@ -0,0 +1,8 @@
+"""Модуль авторизации пользователя(проверки доступа к определнным его возможностям)"""
+
+
+# TODO
+class BotAuthorisationPermissions:
+
+    def registration(self, user_id, chat_id):
+        pass
